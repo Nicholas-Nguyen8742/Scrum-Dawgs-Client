e904bb428e735ffdf9610fe2c9338bd7bcf32558
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-13 16:36:37 -0400

    Initial project setup with some dependencies installed and font assets created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.scss';
+import React from 'react';
+
+function Header() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/pages/LoginPage/LoginPage.jsx
@@ -0,0 +1,11 @@
+import './LoginPage.scss';
+import React from 'react';
+
+export default function LoginPage() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
